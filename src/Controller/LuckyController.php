69aa665f4d4ec4acde0922e2da66d877953c12f2
UrtziFM <urtzi.arana13@gmail.com>
2020-01-28 15:43:57 +0100

    adding new controller and twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,20 +2,21 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky_you")
      */
     public function number()
     {
         $number = random_int(0, 100);
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig',
+        ['number' => $number,
+        ]);
     }
 }
 ?>
